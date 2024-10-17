1677f8acecf590fa35c97579a4ac25a288202a2c
FinnGuo717 <24120606g@connect.polyu.hk>
2024-10-17 13:50:44 +0800

    111
    111

---
+++ assignment/02/homework2.py
@@ -0,0 +1,52 @@
+import turtle
+import random
+import time
+
+width=800
+height=600
+n=33
+d_list=[]
+x_list=[]
+y_list=[]
+sx_list=[]
+sy_list=[]
+c_list=[]
+for i in range(n):
+    d= random.randint(20,180)
+    d_list.append(d)
+    x = random.randint(-width//2, width//2)
+    y = random.randint(-height//2, height//2)
+    x_list.append(x)
+    y_list.append(y)
+    r=random.randint(0,225)
+    g=random.randint(0,225)
+    b=random.randint(0,225)
+    c=(r,g,b)
+    c_list.append(c)
+
+    sx=random.randint(-5,5)
+    sy=random.randint(-5,5)
+    sx_list.append(sx)
+    sy_list.append(sy)
+
+turtle.setup(width,height)
+turtle.tracer(0)
+turtle.hideturtle()
+turtle.up()
+turtle.colormode(255)
+
+while True:
+    turtle.clear()
+    for i in range(n):
+        x_list[i]= x_list[i]+ sx_list[i]
+        y_list[i]= y_list[i]+ sy_list[i]
+        if x_list[i]<=-width//2 or x_list[i]>=width//2:
+            sx_list[i]=-sx_list[i]
+        if y_list[i]<=-height//2 or y_list[i]>=width//2:
+            sy_list[i]=-sy_list[i]
+    
+    for i in range(n):
+        turtle.goto(x_list[i], y_list[i])
+        turtle.dot(d_list[i], c_list[i])
+    turtle.update()
+    time.sleep(0.01)
--- week5/2_gen_image.py
+++ week5/2_gen_image.py
@@ -3,10 +3,9 @@ import torch
 
 model = "runwayml/stable-diffusion-v1-5"
 
-pipe = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.float16)
-pipe.to("cuda")
+pipe = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.float32)
 
 while True:
     prompt = input("Type a prompt and press enter to generate an image:\n>>> ")
-    images = pipe(prompt, num_inference_steps=20).images
+    images = pipe(prompt, num_inference_steps=5).images
     images[0].show()
